115214c53b8913ecdbd0e2b1d0d206476df63f8e
Michal Buczko <michal.buczko@gmail.com>
2015-03-11 11:12:33 +0100

    indentation fixed

--- js/jquery-slidingCarousel.js
+++ js/jquery-slidingCarousel.js
@@ -1,167 +1,167 @@
 (function($) {
-  $.fn.slidingCarousel = function (options) {
-      options = $.extend({}, $.fn.slidingCarousel.defaults, options || {});
+	$.fn.slidingCarousel = function (options) {
+		options = $.extend({}, $.fn.slidingCarousel.defaults, options || {});
 
-      var pluginData = {
-          container: $(this),
-          sinus:   [0],
-          images:  null,
-          mIndex:  null
-      };
+		var pluginData = {
+			container: $(this),
+			sinus:   [0],
+			images:  null,
+			mIndex:  null
+		};
 
-      var preload = function(callback) {
-          var images = pluginData.container.find(".slide img"),
-              total  = images.length,
-			  shift  = total % 2,
-			  middle = total < 3 ? total : ~~(total / 2) + shift,
-              result = [],
-              loaded = 0;
+		var preload = function(callback) {
+			var images = pluginData.container.find(".slide img"),
+				total  = images.length,
+				shift  = total % 2,
+				middle = total < 3 ? total : ~~(total / 2) + shift,
+				result = [],
+				loaded = 0;
 
-          images.each(function (index, element) {
-			  var img = new Image();
+			images.each(function (index, element) {
+				var img = new Image();
 
-              $(img).bind('load error', function () {
-                  loaded++;
+				$(img).bind('load error', function () {
+					loaded++;
 
-                  // push loaded images into regular array.
-				  // to preserve the order array gets constructed so, that elements indexed:
-				  //
-				  //    0 1 2 3 4 5 6
-				  //
-				  // are shifted within destination array by half of total length (-1 depending on parity/oddness):
-				  //
-				  //    6 5 4 0 1 2 3
-				  //
-				  // and finally reversed to get correct scrolling direction.
+					// push loaded images into regular array.
+					// to preserve the order array gets constructed so, that elements indexed:
+					//
+					//    0 1 2 3 4 5 6
+					//
+					// are shifted within destination array by half of total length (-1 depending on parity/oddness):
+					//
+					//    6 5 4 0 1 2 3
+					//
+					// and finally reversed to get correct scrolling direction.
 
-                  result[(index+middle-shift) % total] = element;
+					result[(index+middle-shift) % total] = element;
 
-                  // need ratio for calculating new widths
-                  element.ratio = this.width / this.height;
-                  element.origH = this.height;
+					// need ratio for calculating new widths
+					element.ratio = this.width / this.height;
+					element.origH = this.height;
 
-                  if (loaded == total) {
-                      pluginData.mIndex = middle;
-					  pluginData.sinsum = 0;
-                      pluginData.images = result.reverse();
+					if (loaded == total) {
+						pluginData.mIndex = middle;
+						pluginData.sinsum = 0;
+						pluginData.images = result.reverse();
 
-                      // prepare symetric sinus table
+						// prepare symetric sinus table
 
-                      for (var n=1, freq=0; n<total; n++) {
-                          pluginData.sinus[n] = (n<=middle) ? Math.sin(freq+=(1.6/middle)) : pluginData.sinus[total-n];
+						for (var n=1, freq=0; n<total; n++) {
+							pluginData.sinus[n] = (n<=middle) ? Math.sin(freq+=(1.6/middle)) : pluginData.sinus[total-n];
 
-                          if (n < middle)
-                              pluginData.sinsum += pluginData.sinus[n]*options.squeeze;
-                      }
-                      callback(pluginData.images);
-                  }
-              });
-			  img.src = element.src;
-          });
-      };
+							if (n < middle)
+								pluginData.sinsum += pluginData.sinus[n]*options.squeeze;
+						}
+						callback(pluginData.images);
+					}
+				});
+				img.src = element.src;
+			});
+		};
 
-      var setupCarousel = function() {
-          preload(doLayout);
-          setupEvents();
-      };
+		var setupCarousel = function() {
+			preload(doLayout);
+			setupEvents();
+		};
 
-      var setupEvents = function() {
-          $('#carousel-right').click(function() {
-              var images = pluginData.images;
+		var setupEvents = function() {
+			$('#carousel-right').click(function() {
+				var images = pluginData.images;
 
-              images.splice(0,0,images.pop());
-              doLayout(images, true);
-          });
-          $('#carousel-left').click(function() {
-              var images = pluginData.images;
+				images.splice(0,0,images.pop());
+				doLayout(images, true);
+			});
+			$('#carousel-left').click(function() {
+				var images = pluginData.images;
 
-              images.push(images.shift());
-              doLayout(images, true);
-          });
-      };
+				images.push(images.shift());
+				doLayout(images, true);
+			});
+		};
 
-      var doLayout = function(images, animate) {
-          var mid  = pluginData.mIndex,
-              sin  = pluginData.sinus,
-              posx = 0,
-              diff = 0,
-              width  = images[mid-1].origH * images[mid-1].ratio,  /* width of middle picture */
-              middle = (pluginData.container.width() - width)/2,   /* center of middle picture, relative to container */
-              offset = middle - pluginData.sinsum,                 /* to get the center, all sinus offset that will be added below have to be substracted */
-              height = images[mid-1].origH, top, left, idx, j=1;
+		var doLayout = function(images, animate) {
+			var mid  = pluginData.mIndex,
+				sin  = pluginData.sinus,
+				posx = 0,
+				diff = 0,
+				width  = images[mid-1].origH * images[mid-1].ratio,  /* width of middle picture */
+				middle = (pluginData.container.width() - width)/2,   /* center of middle picture, relative to container */
+				offset = middle - pluginData.sinsum,                 /* to get the center, all sinus offset that will be added below have to be substracted */
+				height = images[mid-1].origH, top, left, idx, j=1;
 
-          // hide description before doing layout
-          pluginData.container.find('.carousel-caption').hide();
+			// hide description before doing layout
+			pluginData.container.find('.carousel-caption').hide();
 
-          $.each(images, function(i, img) {
-              idx = Math.abs(i+1-mid);
-              top = idx * 15;
+			$.each(images, function(i, img) {
+				idx = Math.abs(i+1-mid);
+				top = idx * 15;
 
-              // calculating new width and caching it for later use
-              img.cWidth = (height-(top*2)) * img.ratio;
+				// calculating new width and caching it for later use
+				img.cWidth = (height-(top*2)) * img.ratio;
 
-              diff = sin[i] * options.squeeze;
-              left = posx += (i < mid) ? diff : images[i-1].cWidth + diff - img.cWidth;
+				diff = sin[i] * options.squeeze;
+				left = posx += (i < mid) ? diff : images[i-1].cWidth + diff - img.cWidth;
 
-              var fn = function() {
-                  if (i === mid-1) addDescription($(img));
-              };
+				var fn = function() {
+					if (i === mid-1) addDescription($(img));
+				};
 
-              if (animate) {
-                  $(img).animate({
-                      height   : height - (top*2),
-                      zIndex   : mid-idx,
-                      top      : top,
-                      left     : left+offset,
-                      opacity  : i==mid-1 ? 1 : sin[j++]*0.8
-                  }, options.animate, fn);
-              }
-              else
-              {
-                  $(img).css({
-                      zIndex   : mid-idx,
-                      height   : height - (top*2),
-                      top      : top,
-                      left     : left+offset,
-                      opacity  : 0
-                  }).show().animate({opacity: i==mid-1 ? 1 : sin[j++]*0.8 }, fn);
+				if (animate) {
+					$(img).animate({
+						height   : height - (top*2),
+						zIndex   : mid-idx,
+						top      : top,
+						left     : left+offset,
+						opacity  : i==mid-1 ? 1 : sin[j++]*0.8
+					}, options.animate, fn);
+				}
+				else
+				{
+					$(img).css({
+						zIndex   : mid-idx,
+						height   : height - (top*2),
+						top      : top,
+						left     : left+offset,
+						opacity  : 0
+					}).show().animate({opacity: i==mid-1 ? 1 : sin[j++]*0.8 }, fn);
 
-                  if (options.shadow)
-                      $(img).addClass('shadow');
-              }
-          });
+					if (options.shadow)
+						$(img).addClass('shadow');
+				}
+			});
 
-          if (!animate) {
-              $('#carousel-left').css('left', middle+50);
-              $('#carousel-right').css('left', middle+width-75);
-          }
-      };
+			if (!animate) {
+				$('#carousel-left').css('left', middle+50);
+				$('#carousel-right').css('left', middle+width-75);
+			}
+		};
 
-      var addDescription = function(img) {
-          var caption = img.closest('.slide').find('.carousel-caption');
-          var position = img.position();
+		var addDescription = function(img) {
+			var caption = img.closest('.slide').find('.carousel-caption');
+			var position = img.position();
 
-          caption.css('width', img.width()).css({
-              top: position.top + img.height()-$(caption).height(),
-              left: position.left,
-              opacity: 0
-          }).show().animate({opacity: 0.8});
+			caption.css('width', img.width()).css({
+				top: position.top + img.height()-$(caption).height(),
+				left: position.left,
+				opacity: 0
+			}).show().animate({opacity: 0.8});
 
-      };
+		};
 
-      this.initialize = function () {
-          setupCarousel();
-      };
+		this.initialize = function () {
+			setupCarousel();
+		};
 
-      // Initialize the plugin
-      return this.initialize();
+		// Initialize the plugin
+		return this.initialize();
 
-  };
+	};
 
-  $.fn.slidingCarousel.defaults = {
-      shadow: true,
-      squeeze: 124,
-      animate: 250
-  };
+	$.fn.slidingCarousel.defaults = {
+		shadow: true,
+		squeeze: 124,
+		animate: 250
+	};
 
 })(jQuery);
